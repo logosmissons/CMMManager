87e92143ff6861e4d1c547e11f9096f11d7c3ad9
Harris J Park <hyungpark@logosmissions.com>
2019-07-23 09:53:39 -0500

    filter done 2

--- CMMManager/frmLogCommunication.Designer.cs
+++ CMMManager/frmLogCommunication.Designer.cs
@@ -87,11 +87,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(164, 33);
+            this.label4.Location = new System.Drawing.Point(186, 33);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(219, 25);
+            this.label4.Size = new System.Drawing.Size(173, 25);
             this.label4.TabIndex = 4;
-            this.label4.Text = "Log Communication";
+            this.label4.Text = "Communication";
             // 
             // label5
             // 
